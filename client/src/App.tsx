4cad33f7729329a305c442537ed243c539a07da3
De-real-iManuel <De_real_iManuel@hotmail.com>
2025-09-02 03:33:04 +0000

    conflict

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { useAuth } from "@/hooks/useAuth";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { useAuth } from "./hooks/useAuth";
 import Landing from "@/pages/landing";
 import Home from "@/pages/home";
 import Quests from "@/pages/quests";
